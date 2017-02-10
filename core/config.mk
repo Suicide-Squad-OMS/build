579a2062a74f84f9cc21a496bd046b02dbec415c
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-02-10 08:22:59 +0100

    Fix uber.mk inclusion, cause i'm bad
    and that's what i do when they come for me
    
    # Conflicts:
    #	core/clear_vars.mk
    #	core/uber.mk

--- core/config.mk
+++ core/config.mk
@@ -126,7 +126,6 @@ BUILD_NOTICE_FILE := $(BUILD_SYSTEM)/notice_files.mk
 BUILD_HOST_DALVIK_JAVA_LIBRARY := $(BUILD_SYSTEM)/host_dalvik_java_library.mk
 BUILD_HOST_DALVIK_STATIC_JAVA_LIBRARY := $(BUILD_SYSTEM)/host_dalvik_static_java_library.mk
 
-
 # ###############################################################
 # Parse out any modifier targets.
 # ###############################################################
